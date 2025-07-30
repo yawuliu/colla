fbd6e897b7a76882bb0ab3be9243f37070fd336d
yawuliu <1138824393@qq.com>
2025-07-30 18:35:02 +0800

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import { Button } from 'ant-design-vue';
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n";
 
 import "./style.scss";
+import 'ant-design-vue/dist/reset.css';
 
 // import "./assets/main.css";
 
@@ -14,5 +16,6 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
+app.use(Button);
 
 app.mount("#app");
